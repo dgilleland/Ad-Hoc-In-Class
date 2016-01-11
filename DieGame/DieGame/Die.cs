8f20c50bbe837404f2b2f5421fa0e8ab0bc47721
Dan Gilleland <dgilleland@nait.ca>
2016-01-11 10:02:50 -0700

    Die Game

---
+++ DieGame/DieGame/Die.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieGame
+{
+    public class Die
+    {
+        #region Properties and Fields - What a Die object "looks like"
+        // This is a private field, made static
+        // This field will be shared by all Die instances (objects)
+        private static Random _rnd = new Random();
+
+        // This is an auto-implemented property
+        public int FaceValue { get; private set; }
+
+        // A private field with a public explicitly implemented property
+        private int _sides;
+        public int Sides
+        {
+            get { return _sides; }
+            private set
+            {
+                // The setter is a good place to ensure only valid values
+                // are assigned to the _sides field.
+                if (value != 6 && value != 12)
+                    throw new Exception("Invalid number of sides - only 6 and 12 are allowed");
+                _sides = value;
+            }
+        }
+        #endregion
+
+        #region Constructors and Methods - How a Die object "behaves"
+        // A parameterless constructor
+        public Die()
+        {
+            Sides = 6;
+            Roll();
+        }
+
+        // An overload of the constructor, one that takes in the number of sides for the die.
+        public Die(int numberOfSides)
+        {
+            Sides = numberOfSides;
+            Roll();
+        }
+
+        public void Roll()
+        {
+            // .Next(inclusive lowest range value, exclusive highest range value)
+            FaceValue = _rnd.Next(1, Sides + 1);
+        }
+        #endregion
+    }
+}
---
+++ DieGame/DieGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DieGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dieSize;
+            Console.WriteLine("Enter the number of sides (6 or 12): ");
+            dieSize = int.Parse(Console.ReadLine()); // no validation for now.....
+
+            // Instantiate our Die objects
+            Die firstPlayer = new Die(dieSize);
+            Die secondPlayer;
+            if (dieSize == 6)
+                secondPlayer = new Die();
+            else
+                secondPlayer = new Die(dieSize);
+
+            PlayGame(firstPlayer, secondPlayer);
+        }
+
+        private static void PlayGame(Die firstPlayerDie, Die secondPlayerDie)
+        {
+            firstPlayerDie.Roll();
+            secondPlayerDie.Roll();
+            Console.WriteLine("Results: " + firstPlayerDie.FaceValue + " vs. " + secondPlayerDie.FaceValue);
+            if (firstPlayerDie.FaceValue > secondPlayerDie.FaceValue)
+                Console.WriteLine("First player won");
+            else if (firstPlayerDie.FaceValue < secondPlayerDie.FaceValue)
+                Console.WriteLine("Second player won");
+            else
+                Console.WriteLine("It's a Tie!!");
+        }
+    }
+}
