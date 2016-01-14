193e40beb702f093142c9991058e4fc0a4f8deeb
Dan Gilleland <dgilleland@nait.ca>
2016-01-14 14:19:16 -0700

    Finish Die demo

--- DieGame/MyGame/Form1.Designer.cs
+++ DieGame/MyGame/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.Player1 = new System.Windows.Forms.Label();
             this.Player2 = new System.Windows.Forms.Label();
             this.RollButton = new System.Windows.Forms.Button();
+            this.TurnResultsGridView = new System.Windows.Forms.DataGridView();
+            this.FirstPlayerWinCount = new System.Windows.Forms.Label();
+            this.SecondPlayerWinCount = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.TurnResultsGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // Player1
@@ -47,7 +51,7 @@
             // 
             this.Player2.AutoSize = true;
             this.Player2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Player2.Location = new System.Drawing.Point(144, 13);
+            this.Player2.Location = new System.Drawing.Point(153, 13);
             this.Player2.Name = "Player2";
             this.Player2.Size = new System.Drawing.Size(36, 39);
             this.Player2.TabIndex = 1;
@@ -64,17 +68,55 @@
             this.RollButton.UseVisualStyleBackColor = true;
             this.RollButton.Click += new System.EventHandler(this.RollButton_Click);
             // 
+            // TurnResultsGridView
+            // 
+            this.TurnResultsGridView.AllowUserToAddRows = false;
+            this.TurnResultsGridView.AllowUserToDeleteRows = false;
+            this.TurnResultsGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TurnResultsGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.TurnResultsGridView.Location = new System.Drawing.Point(20, 108);
+            this.TurnResultsGridView.Name = "TurnResultsGridView";
+            this.TurnResultsGridView.ReadOnly = true;
+            this.TurnResultsGridView.Size = new System.Drawing.Size(363, 187);
+            this.TurnResultsGridView.TabIndex = 3;
+            // 
+            // FirstPlayerWinCount
+            // 
+            this.FirstPlayerWinCount.AutoSize = true;
+            this.FirstPlayerWinCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FirstPlayerWinCount.Location = new System.Drawing.Point(20, 65);
+            this.FirstPlayerWinCount.Name = "FirstPlayerWinCount";
+            this.FirstPlayerWinCount.Size = new System.Drawing.Size(40, 24);
+            this.FirstPlayerWinCount.TabIndex = 4;
+            this.FirstPlayerWinCount.Text = "000";
+            // 
+            // SecondPlayerWinCount
+            // 
+            this.SecondPlayerWinCount.AutoSize = true;
+            this.SecondPlayerWinCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SecondPlayerWinCount.Location = new System.Drawing.Point(160, 65);
+            this.SecondPlayerWinCount.Name = "SecondPlayerWinCount";
+            this.SecondPlayerWinCount.Size = new System.Drawing.Size(40, 24);
+            this.SecondPlayerWinCount.TabIndex = 5;
+            this.SecondPlayerWinCount.Text = "000";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(516, 211);
+            this.ClientSize = new System.Drawing.Size(408, 307);
+            this.Controls.Add(this.SecondPlayerWinCount);
+            this.Controls.Add(this.FirstPlayerWinCount);
+            this.Controls.Add(this.TurnResultsGridView);
             this.Controls.Add(this.RollButton);
             this.Controls.Add(this.Player2);
             this.Controls.Add(this.Player1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.TurnResultsGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -85,6 +127,9 @@
         private System.Windows.Forms.Label Player1;
         private System.Windows.Forms.Label Player2;
         private System.Windows.Forms.Button RollButton;
+        private System.Windows.Forms.DataGridView TurnResultsGridView;
+        private System.Windows.Forms.Label FirstPlayerWinCount;
+        private System.Windows.Forms.Label SecondPlayerWinCount;
     }
 }
 
--- DieGame/MyGame/Form1.cs
+++ DieGame/MyGame/Form1.cs
@@ -36,11 +36,45 @@ namespace MyGame
 
             Player1.Text = FirstPlayer.FaceValue.ToString();
             Player2.Text = SecondPlayer.FaceValue.ToString();
+
+            Turn result = new Turn();
+            result.FirstPlayerResult = FirstPlayer.FaceValue;
+            result.SecondPlayerResult = SecondPlayer.FaceValue;
+
+            if (result.FirstPlayerResult > result.SecondPlayerResult)
+                result.Winner = "First Player";
+            else if (result.FirstPlayerResult < result.SecondPlayerResult)
+                result.Winner = "Second Player";
+            else
+                result.Winner = "Tie Game";
+
+            GamesList.Add(result); // Add an item to the end of the List<T> object
+            TurnResultsGridView.DataSource = null; // hack for refreshing the DataGrid
+            TurnResultsGridView.DataSource = GamesList;
+
+            UpdateWinResults();
+        }
+
+        private void UpdateWinResults()
+        {
+            int firstTotal = 0, secondTotal = 0;
+            //      type varName collection
+            //        |    |       |
+            //       \_/  \_/     \_/
+            foreach(Turn item in GamesList)
+            {
+                if (item.Winner == "First Player")
+                    firstTotal++;
+                if (item.Winner == "Second Player")
+                    secondTotal++;
+            }
+            FirstPlayerWinCount.Text = firstTotal.ToString();
+            SecondPlayerWinCount.Text = secondTotal.ToString();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            TurnResultsGridView.DataSource = GamesList;
         }
     }
 }
