f058df70d6fb4010b3cae25876681f9c31950c78
Dan Gilleland <dgilleland@nait.ca>
2016-01-21 15:05:53 -0700

    More Form Input examples

---
+++ RazorDemos/Website/App_Code/BankAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for BankAccount
+/// </summary>
+public class BankAccount
+{
+    public string AccountHolder { get; set; }
+    public string AccountNumber { get; set; }
+    public decimal OpeningBalance { get; set; }
+    public decimal OverdraftLimit { get; set; }
+    public AccountType AccountType { get; set; }
+}
+
+public enum AccountType { Chequing, Savings, Credit }
