ee89effd9920e7e316718048d659b467880c3461
Dan Gilleland <dgilleland@nait.ca>
2016-02-04 15:11:33 -0700

    General C# stuff "on the fly"

---
+++ OnTheFly/ConsoleApplication1/IntExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    static class IntExtensions
+    {
+        public static string Quack(this int self)
+        {
+            string result = "(Quack)";
+            if(self <= 1)
+            {
+                return result;
+            }
+            else
+            {
+                while (self > 1)
+                {
+                    result += " (quack)";
+                    self--;
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ OnTheFly/ConsoleApplication1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 4;
+
+            Console.WriteLine(counter.Quack());
+            int result = AddNumbers(2, 7, 12, 44);
+            Console.WriteLine("The sum of the numbers is " + result);
+
+            int[] numbers = new int[] {1,2,3};
+            Console.WriteLine("The sum of the second set of numbers is " + AddNumbers(numbers));
+            double total = 10 * 10.5 + 3;
+
+            result = AddNumbers(result, AddNumbers(numbers));
+
+            Console.WriteLine(DRY("Hello", 3));
+            string message = DRY(count: 3, message: "Goodbye");
+        }
+
+        static string DRY(string message, int count)
+        {
+            if(count >= 1)
+            {
+                message += " " + message;
+                count--;
+            }
+            return message;
+        }
+
+        static int AddNumbers(params int[] values)
+        {
+            int sum = 0;
+            foreach(int item in values)
+                sum += item;
+            return sum;
+        }
+    }
+}
