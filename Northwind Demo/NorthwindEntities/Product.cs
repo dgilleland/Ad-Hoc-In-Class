5b85213da2352d286f7f73e9844b6c87553d32ad
Dan Gilleland <dgilleland@nait.ca>
2016-03-09 10:11:13 -0700

    Northwind Demo project (in-class)

---
+++ Northwind Demo/NorthwindEntities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindEntities
+{
+    // This class will be mapped directly to the Products table in NorthwindExtended.
+    public class Product
+    {
+        public int ProductID { get; set; }          // ProductID    int     PRIMARY KEY     NOT NULL,
+        public string ProductName { get; set; }     // ProductName  nvarchar(40)            NOT NULL,
+        public int? SupplierID { get; set; }        // SupplierID   int                         NULL,
+        public int? CategoryID { get; set; }        // CategoryID   int                         NULL,
+        public string QuantityPerUnit { get; set; } // QuantityPerUnit  nvarchar(20)            NULL,
+        public decimal? UnitPrice { get; set; }     // UnitPrice    money                       NULL,
+        public short? UnitsInStock { get; set; }    // UnitsInStock smallint                    NULL,
+        public short? UnitsOnOrder { get; set; }    // UnitsOnOrder smallint                    NULL,
+        public short? ReorderLevel { get; set; }    // ReorderLevel smallint                    NULL,
+        public bool Discontinued { get; set; }      // Discontinued bit                     NOT NULL
+    }
+}
---
+++ Northwind Demo/NorthwindSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NorthwindSystem")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NAIT")]
+[assembly: AssemblyProduct("NorthwindSystem")]
+[assembly: AssemblyCopyright("Copyright © NAIT 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cea33b21-4068-495f-8fb3-48a83f64089b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
