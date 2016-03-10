4ad1598eec2bf3274361fb98371b3df2c7e606b0
Dan Gilleland <dgilleland@nait.ca>
2016-03-10 15:04:16 -0700

    DAL and starter BLL for class

---
+++ Northwind Demo/NorthwindSystem/BLL/NorthwindController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Namespace usings for the Entity classes and the Data Access Layer
+using NorthwindEntities;
+using NorthwindSystem.DAL;
+
+namespace NorthwindSystem.BLL
+{
+    // This class is the public access into our system/application
+    // that will be used by the website.
+    public class NorthwindController
+    {
+        public List<Product> ListAllProducts()
+        {
+            // This "using" statement is different than the "using" at the top of this file.
+            // This "using" statement is to ensure that the connection to the database is properly closed after we are done.
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                System.Data.Entity.DbSet<Product> stuff = context.Products;
+                List<Product> listedStuff = stuff.ToList();
+                return listedStuff;
+                //return context.Products.ToList();
+            }
+        }
+    }
+}
---
+++ Northwind Demo/NorthwindSystem/DAL/NorthwindContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+// The Entity Framework (EF) is a library of classes that allow
+// us to interact and exchange data from database tables into
+// classes that we define in our application.
+// If we create classes that mirror the structure of the 
+// database tables, then we can create a "Context" class
+// that mirrors the database as a whole.
+// NorthwindContext <==> NorthwindExtended database
+// Properties of NorthwindContext <==> tables in the database
+// Properties of Entities <==> columns in a specific table
+using System.Data.Entity; // From the EF, for DbContext class
+using NorthwindEntities; // To reference our Entity classes
+
+namespace NorthwindSystem.DAL
+{
+    // We declare the class as "internal" so that it cannot be used
+    // from outside this project's DLL.
+    internal class NorthwindContext : DbContext
+    {
+        // Name of connection string in web.config passed to the base class
+        public NorthwindContext() : base("NW")
+        {
+        }
+
+        // Properties that map our entities to database tables
+        // Notice how the name we choose for our property matches
+        // the name of the database table we are mapping to.
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+    }
+}
