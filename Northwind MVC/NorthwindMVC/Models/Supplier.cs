24a8656d1bbb3b86a06c2f24ab3a6028e26d9b9b
Dan Gilleland <dgilleland@nait.ca>
2016-04-05 10:51:58 -0600

    Quick MVC Sample

---
+++ Northwind MVC/NorthwindMVC/Models/Supplier.cs
@@ -0,0 +1,61 @@
+namespace NorthwindMVC.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Supplier
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Supplier()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int SupplierID { get; set; }
+
+        [Required]
+        [StringLength(40)]
+        public string CompanyName { get; set; }
+
+        [StringLength(30)]
+        public string ContactName { get; set; }
+
+        [StringLength(30)]
+        public string ContactTitle { get; set; }
+
+        [StringLength(60)]
+        public string Address { get; set; }
+
+        [StringLength(15)]
+        public string City { get; set; }
+
+        [StringLength(15)]
+        public string Region { get; set; }
+
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [StringLength(15)]
+        public string Country { get; set; }
+
+        [StringLength(24)]
+        public string Phone { get; set; }
+
+        [StringLength(24)]
+        public string Fax { get; set; }
+
+        [StringLength(100)]
+        public string HomePageTitle { get; set; }
+
+        [StringLength(100)]
+        public string HomePageUrl { get; set; }
+
+        public DateTime LastModified { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
